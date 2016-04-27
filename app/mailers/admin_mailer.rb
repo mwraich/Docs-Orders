5a8b10a5654245685f4f2710254a998714490b1b
mwraich <mwraich28@gmail.com>
2016-04-26 22:03:54 -0400

    Added new registration alert email to admins

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,2 +1,10 @@
 class AdminMailer < ApplicationMailer
+  default from: 'admin@bananas.com'
+
+  def new_registration(user)
+    @user = user
+    mail(subject: "New user signup: #{@user.email}")
+  end
+
+  end
 end
