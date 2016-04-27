2b3263bf8b9a8aed672f962e3aef546123ad00aa
mwraich <mwraich28@gmail.com>
2016-04-26 21:32:26 -0400

    Added welcome email method

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+  default from: 'not_a_real_email@fake.com'
+
+  def welcome_email(user)
+    @user = user
+    @url = 'www.test.com'
+    mail(to: @user.email, subject: 'Welcome!')
+  end
 end
